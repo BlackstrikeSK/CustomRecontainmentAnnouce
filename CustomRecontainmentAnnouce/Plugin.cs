d693ffc4adc68d7e5b8066e5fb2f15db59565046
richa <richa@LAPTOP-V5HFBTA1>
2021-09-02 18:34:37 +0200

    Add project files.

---
+++ CustomRecontainmentAnnouce/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+
+namespace CustomRecontainmentAnnouce
+{
+    public class Plugin : Plugin<PluginConfig>
+    {
+        private static readonly Plugin Singleton = new Plugin();
+
+        public override string Author => "BlackstrikeSK";
+        public override string Name => "CustomRecontainmentAnnouce";
+        public override PluginPriority Priority => PluginPriority.Medium;
+        public override Version RequiredExiledVersion => new Version(2, 14, 0);
+        public override Version Version => new Version(1, 0, 0);
+
+        public static Plugin Instance => Singleton;
+
+
+    }
+}
---
+++ CustomRecontainmentAnnouce/PluginConfig.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Exiled.API.Interfaces;
+
+namespace CustomRecontainmentAnnouce
+{
+    public sealed class PluginConfig : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+    }
+}
