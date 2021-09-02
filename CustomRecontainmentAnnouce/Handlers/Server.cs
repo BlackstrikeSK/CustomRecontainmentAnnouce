e0514e3b4936050c03b18637044c17c0716bba78
BlackstrikeSK <blackstrikesk.yt@gmail.com>
2021-09-02 21:28:06 +0200

    Added handlers To Source

---
+++ CustomRecontainmentAnnouce/Handlers/Server.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Exiled.Events.EventArgs;
+using Exiled.API.Features;
+
+namespace CustomRecontainmentAnnouce.Handlers
+{
+    class Server
+    {
+        public void OnSCPTermination(AnnouncingScpTerminationEventArgs ev)
+        {
+                Log.Info(ev.TerminationCause);
+               /* if(ev.Role.roleId == RoleType.Scp93953)
+                {
+                    ev.IsAllowed = false;
+                    Cassie.Message(Plugin.Instance.Config.SCP939MessageUn, false, false);
+                    
+                }
+
+                if (ev.Role.roleId == RoleType.Scp93989)
+                {
+                    ev.IsAllowed = false;
+                    Cassie.Message(Plugin.Instance.Config.SCP939MessageUn, false, false);
+
+                }
+
+                if (ev.Role.roleId == RoleType.Scp049)
+                {
+                    ev.IsAllowed = false;
+                    Cassie.Message(Plugin.Instance.Config.SCP049MessageUn, false, false);
+
+                }
+
+                if (ev.Role.roleId == RoleType.Scp096)
+                {
+                    ev.IsAllowed = false;
+                    Cassie.Message(Plugin.Instance.Config.SCP096MessageUn, false, false);
+
+                }
+
+                if (ev.Role.roleId == RoleType.Scp173)
+                {
+                    ev.IsAllowed = false;
+                    Cassie.Message(Plugin.Instance.Config.SCP173MessageUn, false, false);
+
+                }
+
+                if (ev.Role.roleId == RoleType.Scp106)
+                {
+                    ev.IsAllowed = false;
+                    Cassie.Message(Plugin.Instance.Config.SCP106MessageUn, false, false);
+
+                }
+
+                if (ev.Role.roleId == RoleType.Scp079)
+                {
+                    ev.IsAllowed = false;
+                    Cassie.Message(Plugin.Instance.Config.SCP079MessageUn, false, false);
+
+                }*/
+        }
+    }
+}
--- CustomRecontainmentAnnouce/PluginConfig.cs
+++ CustomRecontainmentAnnouce/PluginConfig.cs
@@ -6,5 +6,18 @@ namespace CustomRecontainmentAnnouce
     public sealed class PluginConfig : IConfig
     {
         public bool IsEnabled { get; set; } = true;
+
+        // Unspecified Reason Message
+        public string SCP939MessageUn { get; set; } = "";
+
+        public string SCP049MessageUn { get; set; } = "";
+
+        public string SCP096MessageUn { get; set; } = "";
+
+        public string SCP173MessageUn { get; set; } = "";
+
+        public string SCP106MessageUn { get; set; } = "";
+
+        public string SCP079MessageUn { get; set; } = "";
     }
 }
